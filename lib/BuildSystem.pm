96003a4cd3fc66fe7e2983a26353dda625d9e0a7
brian d foy <brian@stonehenge.com>
2008-10-29 15:41:01 -0500

    * Conversion from SVN to git

--- lib/BuildSystem.pm
+++ lib/BuildSystem.pm
@@ -15,7 +15,7 @@ use File::Spec::Functions qw(catfile);
 
 use Module::Extract::VERSION;
 
-$VERSION = '0.10_02';
+$VERSION = '0.11';
 
 =head1 NAME
 
@@ -31,7 +31,7 @@ Distribution::Guess::BuildSystem - This is the description
 		dist_dir => $dir
 		);
 	
-	my $build_files    = $guesser->build_files; # Hash ref
+	my $build_files   = $guesser->build_files; # Hash ref
 	
 	my $build_pl      = $guesser->has_build_pl;
 	my $makefile_pl   = $guesser->has_makefile_pl;
@@ -580,13 +580,9 @@ BEGIN { require Module::Extract::Use }
 
 =head1 SOURCE AVAILABILITY
 
-This source is part of a SourceForge project which always has the
-latest sources in SVN, as well as all of the previous releases.
+This source is in Github:
 
-	http://sourceforge.net/projects/brian-d-foy/
-
-If, for some reason, I disappear from the world, one of the other
-members of the project can shepherd this module appropriately.
+	git://github.com/briandfoy/distribution-guess-buildsystem.git
 
 =head1 AUTHOR
 
